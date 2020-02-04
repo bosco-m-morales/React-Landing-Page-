34cd0509cccd2d88897f21701952080c8bb4eacb
Bosco Moises Morales <bmoises.morales@gmail.com>
2020-02-04 00:51:24 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Navbar } from "./navbar";
+import { Card } from "./Card";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +8,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar />
+			<Card />
+
+			<div className="text-center mt-5">
+				<h1>Hello Rigo!</h1>
+				<p>
+					<img src={rigoImage} />
+				</p>
+				<a href="#" className="btn btn-success">
+					If you see this green button... bootstrap is working
+				</a>
+				<p>
+					Made by{" "}
+					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
+					with love!
+				</p>
+
+				<div className="d-flex justify-content-around">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+		</>
 	);
 }
